a3eb2262d568f5078372a5e0babe9c80f62c9396
TonyMo0310 <152178623+TonyMo0310@users.noreply.github.com>
2024-12-23 19:05:05 +0800

    Update lighting_controller.v

--- kitchen_hood_final.srcs/sources_1/new/lighting_controller.v
+++ kitchen_hood_final.srcs/sources_1/new/lighting_controller.v
@@ -1,21 +1,21 @@
 `timescale 1ns / 1ps
 
 module lighting_controller (
-    input wire clk,
-    input wire rst_n,
-    input wire power_state,
-    input wire lighting_switch,    // Switch input instead of button
-    output reg lighting_state
+    input wire clk,                 // Clock input
+    input wire rst_n,              // Active low reset input
+    input wire power_state,         // Power state signal
+    input wire lighting_switch,     // Switch input instead of button
+    output reg lighting_state       // Output for lighting state
 );
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-        lighting_state <= 0;
+        lighting_state <= 0;        // Reset lighting state to off
     end
     else if (!power_state) begin
-        lighting_state <= 0;
+        lighting_state <= 0;        // Turn off lighting if power is off
     end
     else begin
-        lighting_state <= lighting_switch;
+        lighting_state <= lighting_switch; // Set lighting state based on switch input
     end
 end
 endmodule
